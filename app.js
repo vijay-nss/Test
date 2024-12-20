6bb3c77552c0bff90aeafd4265399ff0f74393bb
vijay.honeybill <vijay.honeybill@gmail.com>
2024-12-20 12:31:14 +0530

    atlas url added

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dataRouter = require('./router/data.router');
+require('dotenv').config()
 
 const app = express();
-const PORT = 3000;
-const MONGO_URI = 'mongodb://localhost:27017/DataStore';
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
+console.log(MONGO_URI);
 
 app.use(express.json());
 app.use('/api', dataRouter);
 
 mongoose
-    .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .connect(MONGO_URI)
     .then(() => {
         console.log('Connected to MongoDB');
-        app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+        app.listen(PORT, () => console.log(`Server running on ${MONGO_URI}`));
     })
     .catch((error) => console.error('MongoDB connection error:', error));
